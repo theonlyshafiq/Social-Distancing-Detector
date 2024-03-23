620b41184a6c98f6db5271348be7b986b8b3d6c2
Ahmed Shafiq <ahmedmohamedshafiq12@gmail.com>
2024-03-23 02:32:01 +0200

    Adding output video

--- src/Detector.py
+++ src/Detector.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 class Detector:
     def __init__(self):
-        self.model = YOLO('weights/yolov8s.pt')
+        self.model = YOLO('weights/yolov8m.pt')
 
     def find_people(self, frame):
         results = self.model(frame, classes=[0], conf=0.5)[0]
